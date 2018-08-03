2158272bd3aa3bdcdaba9d07811939f58cac0a26
flak <ali76034@gmail.com>
2018-08-02 20:05:41 -0400

    stuff

---
+++ detect.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url = "http://neocrisis.xyz/telescope/"
+
+
+def get_positions():
+    objects = []
+    for x in range(1 , 9):
+        response = requests.get(url+str(x))
+        loaded = json.loads(response.text)
+        for obj in loaded['objects']:
+            if obj["type"] == "rock":
+                objects.append(tuple((obj["name"], obj["obs_time"], obj["pos"])))
+    return objects
+
+first = sorted(get_positions())
+second = sorted(get_positions())
+
+print(first)
+print(second)
+zipped = zip(first, second)
+
+print(json.dumps(list(zipped), indent= 2))
